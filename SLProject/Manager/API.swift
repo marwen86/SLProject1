52b04d9f11dbf62e734cf515498733af2e369f54
marouane youssef <marouane_youssef@gcarrefour.com>
2017-10-23 01:40:49 +0200

    generique Url

--- SLProject/Manager/API.swift
+++ SLProject/Manager/API.swift
@@ -7,32 +7,18 @@
 //
 
 import Foundation
+let API_BASE_URL = ""
+let API_KEY = ""
+
 typealias APIRequestResponse = (Data?, URLResponse?, Error?) -> ()
 struct API {
     
-    private struct Constants {
-        static let APPID = "6a700a1e919dc96b0a98901c9f4bec47"
-        static let baseURL = "http://api.openweathermap.org/"
-    }
-    
-    enum ResourcePath: String {
-        case Forecast = "data/2.5/forecast/daily?q=paris&mode=Json&units=metric&cnt=16&appid="
-        case Icon = "img/w/"
-    
-        var path: String {
-            return Constants.baseURL + rawValue
-        }
-        
-        var forecastpath: String {
-            return Constants.baseURL + rawValue + Constants.APPID
-        }
-    }
-    
     static func loadData(fromUrl url:String? , response : @escaping APIRequestResponse) {
-        
-        guard let url  = url ,  let requestURL = URL(string: url) else {
+       
+        guard let requestURL = API.generateCurrentRequestURL(param: "user") else {
             return
         }
+
         let urlRequest = URLRequest(url: requestURL)
         
         let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: response)
@@ -61,6 +47,19 @@ struct API {
         task.resume()
        
     
+    }
+}
+
+extension API {
+    
+    static func generateCurrentRequestURL(param : String) -> URL? {
+        guard var components = URLComponents(string:API_BASE_URL) else {
+            return nil
+        }
+        
+        components.queryItems = [URLQueryItem(name:"Key", value:param),
+                                 URLQueryItem(name:"key", value:API_KEY)]
+        return components.url
     }
 }
 
